0d6fa20f1260dd5fa1528cd19d53f16a8fc786c8
Mike Fajardo Turner <miguelstrife@gmail.com>
2025-07-24 18:51:47 -0600

    Feature/electron forge integration (#2)
    * Working 1st packaged build
    
    * update on forge.config multiplatform

--- src/index.js
+++ src/index.js
@@ -24,6 +24,8 @@ app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
 })
 
+if (require('electron-squirrel-startup')) app.quit();
+
 app.whenReady().then(() => {
   createWindow();
 
